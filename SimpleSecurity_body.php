e618616adb37c63586302c5708f7a45f5f07449b
rlot <rj23@protonmail.com>
2016-12-14 22:05:15 +0100

    "Article::getContent()" is deprecated in MediaWiki 1.21
    Replaced "Article::getContent()" usage
    
    Bug: T151973
    Change-Id: I0fba699b211baacdeaceeb20aa6c39d85339248e

--- SimpleSecurity_body.php
+++ SimpleSecurity_body.php
@@ -48,7 +48,8 @@ class SimpleSecurity {
 		# Add extra available groups if $wgSecurityGroupsArticle is set
 		if ( $wgSecurityGroupsArticle ) {
 			$groups = new Article( Title::newFromText( $wgSecurityGroupsArticle, NS_MEDIAWIKI ) );
-			if ( preg_match_all( "/^\*?\s*(.+?)\s*(\|\s*(.+))?$/m", $groups->getContent(), $match ) ) {
+			$groupsContent = $groups->getContentObject();
+			if ( preg_match_all( "/^\*?\s*(.+?)\s*(\|\s*(.+))?$/m", ContentHandler::getContentText( $groupsContent ), $match ) ) {
 				foreach( $match[1] as $i => $k ) {
 					$v = $match[3][$i];
 					if ( $v ) $wgSecurityExtraGroups[strtolower( $k )] = $v;
